216eb8366116700c44fa8a5cadc944b56fabddce
Hoperaisers <abdallahsafari@gmail.com>
2023-05-23 16:06:52 +0300

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Counter from "./components/Counter";
+import Counters from "./components/Asynchronous";
 
 ReactDOM.render(<Counter />, document.getElementById("root"));
+ReactDOM.render(<Counters />, document.getElementById("root"));
